230a7bbf4704c0eaa5c7ec57519fb22c3a042f7d
Stefan Codrescu <ssmmcc1@gmail.com>
2017-03-10 21:47:22 +0000

    removing initial code from Meg

--- source/goesr_l1b_concat_wrapper.sh
+++
@@ -1,8 +0,0 @@
-
-#!/usr/bin/env bash
-
-# wrapper script to execute python L1b file aggregation routine.
-
-export PYTHONPATH=$PYTHONPATH:/data/backup/GOES_R/goes-r/l2/common/goesr/data
-
-python goesr_l1b_concat.py l1b_concat.json -d 2016-09-26
