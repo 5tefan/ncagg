1e569c4db61b4bf40a01c0dc28dfa9c7e40c5e5a
Stefan Codrescu <ssmmcc1@gmail.com>
2017-06-19 16:31:43 +0000

    Agg to tmpfile, then rename to final when finished

--- aggregoes/ncei/ncei_l1b_cli.py
+++ aggregoes/ncei/ncei_l1b_cli.py
@@ -1,12 +1,16 @@
 from aggregoes.cli import ProgressAggregator as Aggregator
+from aggregoes.ncei.BufferedEmailHandler import BufferedEmailHandler
 from aggregoes.ncei.ncei_l1b_mapper import get_files_for, get_product_config, get_runtime_config, get_output_filename
 from aggregoes.ncei.ncei_l1b_mapper import mapping
 from datetime import datetime, timedelta
+import tempfile
 import click
 import logging
 import os
-from aggregoes.ncei.BufferedEmailHandler import BufferedEmailHandler
 import atexit
+
+logger = logging.getLogger(__name__)
+
 
 @click.group()
 def cli():
@@ -22,12 +26,15 @@ def cli():
 def agg_day(yyyymmdd, product, sat="goes16", env="OR", email=list()):
 
     if len(email) > 0:
+        # If any emails are specified with --email or -e options, set up an email handler
+        # that will collect logging.error("msg") calls and email them to the recipient atexit.
+        logger.debug("Configuring email error handler with recipients: %s" % email)
         hostname = os.environ.get("HOSTNAME", "")
         username = os.environ.get("USER", "aggregation")
         email_handler = BufferedEmailHandler("%s@%s" % (username, hostname), email,
                                              "Aggregation errors - %s %s %s %s" % (yyyymmdd, product, sat, env))
         logging.getLogger().addHandler(email_handler)
-        atexit.register(email_handler.finalize)
+        atexit.register(email_handler.finalize)  # on exit, send the queued error messages
 
     start_time = datetime.strptime(yyyymmdd, "%Y%m%d")
     end_time = start_time + timedelta(days=1) - timedelta(microseconds=1)
@@ -37,8 +44,9 @@ def agg_day(yyyymmdd, product, sat="goes16", env="OR", email=list()):
     files = get_files_for(sat, product, start_time - timedelta(days=1), env)[-60:]
     files += get_files_for(sat, product, start_time, env)
     files += get_files_for(sat, product, start_time + timedelta(days=1), env)[:60]
+    logger.debug("Found %s files total" % len(files))
 
-    # Step 1: initialize the aggregator.
+    # Initialize the aggregator.
     product_config = get_product_config(product)
     a = Aggregator(config=product_config)
 
@@ -48,15 +56,19 @@ def agg_day(yyyymmdd, product, sat="goes16", env="OR", email=list()):
         "max": end_time
     })
 
-    # Step 2: generate the aggregation list
+    # Generate the aggregation list.
     aggregation_list = a.generate_aggregation_list(files, runtime_config)
-    output_filename = get_output_filename(sat, product, yyyymmdd, env)
+    logger.debug("Aggregation list contains %s items" % len(aggregation_list))
 
-    # Step 3: evaluate the aggregation list
-    click.echo("Evaluating aggregation list...")
-    a.evaluate_aggregation_list(aggregation_list, output_filename)
+    # Evaluate it to a temporary working file.
+    logger.info("Evaluating aggregation list...")
+    _, tmp_filename = tempfile.mkstemp(prefix="agg_%s_%s" % (product, yyyymmdd))
+    a.evaluate_aggregation_list(aggregation_list, tmp_filename)
 
-    click.echo("Finished: %s" % output_filename)
+    # Rename (atomicish move) it to the final filename.
+    final_filename = get_output_filename(sat, product, yyyymmdd, env)
+    os.rename(tmp_filename, final_filename)
+    logger.info("Finished: %s" % final_filename)
 
 
 if __name__ == "__main__":
