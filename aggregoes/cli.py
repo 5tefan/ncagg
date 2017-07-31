4a9ced70b5c22e65328847e0b98969ab86f99145
Stefan <Stefan.Codrescu@noaa.gov>
2017-07-31 11:48:27 -0600

    More sophisticated bounds support in main cli

--- aggregoes/cli.py
+++ aggregoes/cli.py
@@ -1,6 +1,6 @@
 from aggregoes.aggregator import Aggregator
 import click
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 
 
@@ -21,19 +21,70 @@ def parse_time(dt_str):
     :return: interpreted datetime
     """
     year = int(dt_str[:4])
-    month = int(dt_str[4:6])
+    month = int(dt_str[4:6] or 1)
     day = int(dt_str[6:8] or 1)
     hour = int(dt_str[8:10] or 0)
     minute = int(dt_str[10:12] or 0)
     return datetime(year, month, day, hour, minute)
+
+def parse_bound_arg(b):
+    """
+    Parse a -b argument specifying bounds of aggregation. Bounds are min:max or Tstart[:[T]stop].
+    start and stop are of the form YYYY[MM[DD[HH[MM]]]]
+    :param b:
+    :return:
+    """
+    b_split = b.split(":")
+    if b_split[0].startswith("T"):
+        # parse as a time indication, cut out the T before sending to parse
+        if len(b_split) == 2:
+            b_split[0] = parse_time(b_split[0][1:])
+            # friendly cli: for the second bound, ignore whether it starts with a T or not.
+            b_split[1] = parse_time(b_split[1][1:] if b_split[1].startswith("T") else b_split[1])
+        elif len(b_split) == 1:
+            # if there's only one, infer dayfile, monthfile, or yearfile based on the length
+            if len(b_split[0][1:]) == 4:  # infer -bTYYYY:TYYYY+1
+                b_split[0] = parse_time(b_split[0][1:])
+                b_split.append(datetime(b_split[0].year+1, 1, 1) - timedelta(microseconds=1))
+            elif len(b_split[0][1:]) == 6:  # infer -bTYYYYMM:TYYYYMM+1
+                b_split[0] = parse_time(b_split[0][1:])
+                # datetime month must be in 1..12, so if month+1 == 13, increment year
+                next_month = b_split[0].month + 1
+                if next_month > 12:
+                    assert next_month == 13
+                    next_year = b_split[0].year + 1
+                    next_month = 1
+                else:
+                    next_year = b_split[0].year
+                b_split.append(datetime(next_year, next_month, 1) - timedelta(microseconds=1))
+            elif len(b_split[0][1:]) == 8:  # infer -bTYYYYMMDD:TYYYYMMDD+1
+                b_split[0] = parse_time(b_split[0][1:])
+                b_split.append(b_split[0] + timedelta(days=1) - timedelta(microseconds=1))
+            elif len(b_split[0][1:]) == 10:  # infer -bTYYYYMMDDHH:TYYYYMMDDHH+1
+                b_split[0] = parse_time(b_split[0][1:])
+                b_split.append(b_split[0] + timedelta(hours=1) - timedelta(microseconds=1))
+            elif len(b_split[0][1:]) == 12:  # infer -bTYYYYMMDDHHMM:TYYYYMMDDHHMM+1
+                b_split[0] = parse_time(b_split[0][1:])
+                b_split.append(b_split[0] + timedelta(minutes=1) - timedelta(microseconds=1))
+        else:
+            raise click.BadParameter("")
+    else:
+        if not len(b_split) == 2: raise click.BadParameter("Expected min:max format.", param="-b")
+        # otherwise convert to numerical
+        b_split = map(float, b_split)
+
+    assert len(b_split) == 2
+    return b_split
 
 
 @click.command()
 @click.argument("dst", type=click.Path(exists=False))
 @click.argument("src", nargs=-1, type=click.Path(exists=True))
 @click.option("-u", help="Give an Unlimited Dimension Configuration as udim:ivar[:hz[:hz]]")
-@click.option("-b", help="If -u given, specify bounds for ivar as min:max. min and max should be numbers, or "
-                         "start with T to indicate a time and then should be TYYYYMMDD[HH[MM]] format.")
+@click.option("-b", help="If -u given, specify bounds for ivar as min:max or Tstart[:[T]stop]. "
+                         "min and max are numerical, otherwise T indicates start and stop are times."
+                         "start and stop are of the form YYYY[MM[DD[HH[MM]]]] and of stop is omitted,"
+                         "it will be inferred to be the least significantly specified date + 1.")
 def cli(dst, src, u=None, b=None):
     runtime_config = {}
     if u is not None:
@@ -47,18 +98,9 @@ def cli(dst, src, u=None, b=None):
             runtime_config[u_split[0]]["expected_cadence"] = {u_split[0]: float(u_split[2])}
 
         if b is not None:
-            b_split = b.split(":")
-            if not len(b_split) == 2: raise click.BadParameter("Expected min:max format.", param="-b")
-            if b_split[0].startswith("T"):
-                # parse as a time indication, cut out the T before sending to parse
-                b_split[0] = parse_time(b_split[0][1:])
-                b_split[1] = parse_time(b_split[1][1:])
-            else:
-                # otherwise convert to numerical
-                b_split = map(float, b_split)
-
-            runtime_config[u_split[0]]["min"] = b_split[0]
-            runtime_config[u_split[0]]["max"] = b_split[1]
+            start, stop = parse_bound_arg(b)
+            runtime_config[u_split[0]]["min"] = start
+            runtime_config[u_split[0]]["max"] = stop
 
     # Step 2: generate the aggregation list
     a = ProgressAggregator()
---
+++ test/generic/test_cli_functionality.py
@@ -0,0 +1,38 @@
+import unittest
+from aggregoes.cli import parse_bound_arg
+from datetime import datetime, timedelta
+from itertools import permutations
+
+years = {str(y): datetime(y, 01, 01) for y in xrange(2010, 2020)}
+
+months = {d.strftime("%Y%m"): d for d in [datetime(2015, m, 01) for m in xrange(1, 12)]}
+
+days = {d.strftime("%Y%m%d"): d for d in [datetime(2016, 01, x) for x in xrange(1,30)]}
+
+adjust = timedelta(microseconds=1)
+
+class TestBoundArgParsing(unittest.TestCase):
+
+    def test_one(self):
+        start, stop = parse_bound_arg("T2017:2019")
+        self.assertEqual(start, datetime(2017, 01, 01))
+        self.assertEqual(stop, datetime(2019, 01, 01))
+
+    def test_year_edge(self):
+        start, stop = parse_bound_arg("T201312")
+        self.assertEqual(start, datetime(2013, 12, 01))
+        self.assertEqual(stop, datetime(2014, 01, 01) - adjust)
+
+    def test_many_times_with_start_and_stop(self):
+        for a, b in permutations(years.items() + months.items() + days.items(), 2):
+            start, stop = parse_bound_arg("T%s:T%s" % (a[0], b[0]))
+            self.assertEqual(start, a[1])
+            self.assertEqual(stop, b[1],"T%s" % b[0] + ", %s != %s" % (stop, b[1]))
+
+    def test_many_start_days(self):
+        for a, b in days.items():
+            start, stop = parse_bound_arg("T%s" % a)
+            self.assertEqual(start, b)
+            self.assertEqual(stop, b + timedelta(days=1) - adjust)
+
+
